f3fed0d92712299ab8ad28dfbbbe084cdb84a9a2
Mohamed Mohamed <mohamedlagha27@gmail.com>
2022-08-16 00:49:44 +0100

    EmployeeDTO(csvInput) constructor has been integrated into the first constructor - EmployeeDAO class moved to under dao package

--- src/main/java/com/sparta/ml/App.java
+++ src/main/java/com/sparta/ml/App.java
@@ -1,43 +1,13 @@
 package com.sparta.ml;
 
-import com.sparta.ml.dto.EmployeeDAO;
-import com.sparta.ml.dto.EmployeeDTO;
+import com.sparta.ml.dao.EmployeeDAO;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
+//Iteration - Filtering
 public class App {
     public static void main( String[] args ) {
         EmployeeDAO.PopulatedArray("src/main/resources/EmployeeRecords.csv");
         System.out.println(EmployeeDAO.getEmployees().size());
+        System.out.println(EmployeeDAO.getEmployees().get(0).getLastName());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
