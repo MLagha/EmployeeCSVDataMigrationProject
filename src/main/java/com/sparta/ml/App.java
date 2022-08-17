2b0927fe1644d99504a85c84ea31e2cf08d06f8c
Mohamed Lagha <mohamedlagha27@gmail.com>
2022-08-17 09:24:39 +0100

    added ConnectionManager, SQLQueries, PropertiesLoader

--- src/main/java/com/sparta/ml/App.java
+++ src/main/java/com/sparta/ml/App.java
@@ -3,11 +3,24 @@ package com.sparta.ml;
 
 import com.sparta.ml.dao.EmployeeDAO;
 
+import java.sql.*;
+
 
 //Iteration - Filtering
 public class App {
     public static void main( String[] args ) {
         EmployeeDAO.populateHashMap("src/main/resources/EmployeeRecords.csv");
         System.out.println(EmployeeDAO.getEmployeesMap().size());
+
+
+
+        Connection postgresConn = ConnectionManager.connectToDB();
+        EmployeeDAO employeeDAO  = new EmployeeDAO(postgresConn);
+        employeeDAO.createEmployee(12343,  "Mr", "fvsdca", "fevdsa", "fvsda", "M", "djew@spartaglobal.com", "9/21/1982", "9/21/2022", "102000");
+        //employeeDAO.createEmployee(98765,  "Ms", "ijuhg", "nbvc", "lkjg", "F", "djew@spartaglobal.com", "9/21/1994", "8/15/2020", "180570");
+        ConnectionManager.closeConnection();
+
+
+
     }
 }
