cde028e73e6e2d7e54287dce9e1eb1810a7d9b73
Jimmy <ngkwoklung@gmail.com>
2022-08-17 19:35:49 +0100

    Clean up some codes

--- src/main/java/com/sparta/ml/App.java
+++ src/main/java/com/sparta/ml/App.java
@@ -7,16 +7,15 @@ import java.sql.*;
 public class App {
 
     public static void main( String[] args ) {
-        EmployeeDAO.populateHashMap("src/main/resources/EmployeeRecords.csv");
         Connection postgresConn = ConnectionManager.connectToDB();
         EmployeeDAO employeeDAO  = new EmployeeDAO(postgresConn);
+        employeeDAO.populateHashMap("src/main/resources/EmployeeRecords.csv");
         try {
             employeeDAO.createEmployeeTable();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        EmployeeDAO.employeeMapToSQL();
-
+        employeeDAO.employeeMapToSQL();
         ConnectionManager.closeConnection();
     }
 }
--- src/main/java/com/sparta/ml/SQLQueries.java
+++ src/main/java/com/sparta/ml/SQLQueries.java
@@ -2,12 +2,11 @@ package com.sparta.ml;
 
 //Execute a statement (for DROP, CREATE, INSERT, UPDATE, or DELETE statements)
 public interface SQLQueries {
-    public static final String SELECT_ALL = "SELECT * from public.employee_db";         //no need for this line!!
 
+    public static final String SELECT_ALL = "SELECT * from public.employee_db";         //no need for this line!!
     String DROP_TABLE = "DROP TABLE IF EXISTS public.employee_db";
     String CREATE_TABLE = "CREATE TABLE public.employee_db";
-    String INSERT_INTO_DB =  "INSERT INTO public.employee_db (Emp_ID, Name_Prefix, First_Name, Middle_Initial, Last_Name, Gender, E_Mail, Date_of_Birth, Date_of_Joining, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-
+    String INSERT_INTO_DB =  "INSERT INTO public.employee_db (Emp_ID, Name_Prefix, First_Name, Middle_Initial" +
+            ", Last_Name, Gender, E_Mail, Date_of_Birth, Date_of_Joining, Salary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 }
