8df060cb521695b768559cd61c261ca633647595
Dovydas Rimkus <drdovy@hotmail.com>
2022-08-18 12:05:55 +0100

    Potentially working multithreading also making everything static

--- src/main/java/com/sparta/ml/ThreadedJDBC.java
+++ src/main/java/com/sparta/ml/ThreadedJDBC.java
@@ -4,6 +4,7 @@ import com.sparta.ml.dao.EmployeeDAO;
 import com.sparta.ml.dto.EmployeeDTO;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,38 +24,40 @@ public class ThreadedJDBC implements Runnable {
 //            emp.insertEmployee(employee, connection);
 //        }
     }
-    Connection postgresConn = ConnectionManager.connectToDB();
-    EmployeeDAO employeeDAO = new EmployeeDAO(postgresConn);
-    Map<String,EmployeeDTO> hmap = employeeDAO.getEmployeesMap();
-    HashMap<String,EmployeeDTO> halfhmap1=new HashMap<>();
-    HashMap<String,EmployeeDTO> halfhmap2=new HashMap<>();
-    void splitHashMap() {
+    static Connection postgresConn = ConnectionManager.connectToDB();
+    static EmployeeDAO employeeDAO = new EmployeeDAO(postgresConn);
+    static Map<String,EmployeeDTO> hmap = employeeDAO.getEmployeesMap();
+    static HashMap<String,EmployeeDTO> halfhmap1 = new HashMap<>();
+    static HashMap<String,EmployeeDTO> halfhmap2 = new HashMap<>();
+    public static void splitHashMap() {
         employeeDAO.populateHashMap("src/main/resources/EmployeeRecords.csv");
         int count = 0;
         for (Map.Entry<String, EmployeeDTO> entry : hmap.entrySet()) {
             (count < (hmap.size() / 2) ? halfhmap1 : halfhmap2).put(entry.getKey(), entry.getValue());
             count++;
         }
-        employeeDAO.employeeMapToSQL(halfhmap1);
-        Thread thread = new Thread(() -> employeeDAO.employeeMapToSQL(halfhmap1));
-        thread.start();
     }
 
     public static void main(String[] args) {
+        long start = System.nanoTime();
         Connection postgresConn = ConnectionManager.connectToDB();
         EmployeeDAO employeeDAO = new EmployeeDAO(postgresConn);
+        try {
+            employeeDAO.createEmployeeTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         Map<String,EmployeeDTO> hmap = employeeDAO.getEmployeesMap();
         employeeDAO.populateHashMap("src/main/resources/EmployeeRecords.csv");
-        HashMap<String,EmployeeDTO> halfhmap1=new HashMap<>();
-        HashMap<String,EmployeeDTO> halfhmap2=new HashMap<>();
-        int count = 0;
-        for (Map.Entry<String, EmployeeDTO> entry : hmap.entrySet()) {
-            (count < (hmap.size() / 2) ? halfhmap1 : halfhmap2).put(entry.getKey(), entry.getValue());
-            count++;
-        }
-        employeeDAO.employeeMapToSQL(halfhmap1);
+        splitHashMap();
+//        employeeDAO.employeeMapToSQL(halfhmap1);
+        System.out.println(halfhmap1.size());
         Thread thread = new Thread(() -> employeeDAO.employeeMapToSQL(halfhmap1));
+        Thread thread2 = new Thread(() -> employeeDAO.employeeMapToSQL(halfhmap2));
         thread.start();
+        thread2.start();
+            double end = System.nanoTime();
+        System.out.println("\nTime taken to persist to SQL table before implementing multiple threads: " + (end - start)/1_000_000_000 + " seconds");
     }
 
 }
